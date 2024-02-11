13acdf9a1f9935b94a047ed638b4db2f649b17f1
LorinczAks <akoslorincz00@gmail.com>
2024-02-11 17:27:47 +0100

    started implementing youtube integration

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+# This example requires the 'message_content' intent.
 
-print("placeholder")
+import discord
+from yt_dlp import YoutubeDL
+#from youtubesearchpython import VideosSearch
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+# setting up variables
+
+music_queue = []
+ytdl = YoutubeDL({'format': 'bestaudio/best'})
+
+vc = None
+is_playing = False
+is_paused = False
+
+
+
+# methods
+def search_yt(item):
+    if item.startswith("https://"):
+        title = ytdl.extract_info(item, download=False)["title"]
+        print(title)
+        return{'source':item, 'title':title}
+    
+    #search = VideosSearch(item, limit=1)
+    #return{'source':search.result()["result"][0]["link"], 'title':search.result()["result"][0]["title"]}
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/play'):
+        url = message.content.split(" ")[1]
+        #print(f'{message.content}, {url}')
+        await message.channel.send(f'Added {search_yt(url)['title']} to queue')
+
+client.run("MTIwNTg0OTA2NDc5MzU3MTM1OA.GRG_qu.5tugjs6pCzCfzZ097YeIQkFAdxGHF7WndJfBkU")
