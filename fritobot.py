5cc669b66e8672524544e4329e4fcfa69c827d30
LorinczAks <akoslorincz00@gmail.com>
2024-02-18 16:52:31 +0100

    implemented several commands

--- fritobot.py
+++ fritobot.py
@@ -1,8 +1,12 @@
 from decouple import config
 import discord
 from yt_dlp import YoutubeDL
+import youtubesearchpython
+import asyncio
 
-
+FFMPEG_OPTIONS = {'options': '-vn',
+                  "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+                  }
 
 
 class fritobot(discord.Client):
@@ -17,14 +21,30 @@ class fritobot(discord.Client):
         self.is_paused = False
 
 
-# methods
-def search_yt(self, item):
-    if item.startswith("https://"):
-        title = self.ytdl.extract_info(item, download=False)["title"]
-        print(title)
-        return{'source':item, 'title':title}
-    
-    #search = VideosSearch(item, limit=1)
-    #return{'source':search.result()["result"][0]["link"], 'title':search.result()["result"][0]["title"]}
+    # methods
+    def search_yt(self, item):
+        if item.startswith("https://"):
+            title = self.ytdl.extract_info(item, download=False)["title"]
+            #print(title)
+            return{'source':item, 'title':title}
+        
+        search = youtubesearchpython.VideosSearch(item, limit=1)
+        result_dict = youtubesearchpython.VideosSearch(item, limit=10)
+        print(f"---------------------------------------------\n{[f"{item["title"]}" for item in result_dict.result()["result"]]}\n----------------------------------------------------------\n")
+        return{'source':search.result()["result"][0]["link"], 'title':search.result()["result"][0]["title"]}
 
+
+    async def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            tmp_url = self.music_queue[0][0]
+
+            self.music_queue.pop(0)
+            loop = asyncio.get_event_loop()
+            data = self.ytdl.extract_info(tmp_url, download=False)
+            self.vc.play(discord.FFmpegPCMAudio(data['url'], executable= "ffmpeg.exe", **FFMPEG_OPTIONS),
+                         after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(), loop))
+        else:
+            self.is_playing = False
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import fritobot
 import discord
 from decouple import config
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,7 +11,9 @@ intents.message_content = True
 client = fritobot.fritobot(intents=intents)
 BOT_TOKEN = config('BOT_TOKEN')
 
-FFMPEG_OPTIONS = {'options': '-vn'}
+FFMPEG_OPTIONS = {'options': '-vn',
+                  "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+                  }
 
 @client.event
 async def on_ready():
@@ -23,22 +26,69 @@ async def on_message(message):
 
     if message.content.startswith('/play'):
         if not client.is_playing:
-            url = message.content.split(" ")[1]
+            query = ' '.join(message.content.split(" ")[1:])
             # to join voice channel and start playing the song
+            # TODO: if user is not in voice channel
+            if message.author.voice == None:
+                await message.channel.send("```Join a voice channel to use bot!```")
+                return
             channel_to_join = message.author.voice.channel
-            client.vc = await channel_to_join.connect()
-
-            data = client.ytdl.extract_info(url, download=False)
-            song = data['url']
-            client.vc.play(discord.FFmpegPCMAudio(song, executable= "ffmpeg.exe", **FFMPEG_OPTIONS))
-            client.is_playing = True
+            if client.vc == None:
+                client.vc = await channel_to_join.connect()
+            
+            song = client.search_yt(query)
+            data = client.ytdl.extract_info(song['source'], download=False)
+            client.music_queue.append((data['url'], data['title']))
+            await message.channel.send(f"```Playing {data['title']}```")
+            await client.play_next()
         else:
             # its already playing so we append the song to the queue
-            url = message.content.split(" ")[1]
-            await message.channel.send(f'Added {client.search_yt(url)['title']} to queue')
-            client.music_queue.append(client.search_yt(url)['source'])
-
-
+            query = ' '.join(message.content.split(" ")[1:])
+            song = client.search_yt(query)
+            data = client.ytdl.extract_info(song['source'], download=False)
+            await message.channel.send(f"```Added {data['title']} to queue```")
+            client.music_queue.append((data['url'], data['title']))
+    elif message.content.startswith('/queue'):
+        if len(client.music_queue) == 0:
+            await message.channel.send("```There are currently no songs in queue```")
+        else:
+            await message.channel.send(f"```Song queue:\n{'\n'.join([f'{item[1]}' for item in client.music_queue])}```")
+    elif message.content.startswith('/stop'):
+        client.music_queue.clear()
+        client.vc.stop()
+        await asyncio.sleep(1)
+        await client.vc.disconnect()
+        client.vc = None
+        await message.channel.send("```Deleted queue and left voice channel. Goodbye! <3```")
+        client.is_playing = False
+    elif message.content.startswith('/skip'):
+        client.vc.stop()
+        await message.channel.send("```Skipped current song```")
+        await asyncio.sleep(1)
+        await client.play_next()
+    elif message.content.startswith('/pause'):
+        if client.is_playing:
+            await message.channel.send("```Paused playback```")
+            client.is_playing = False
+            client.is_paused = True
+            client.vc.pause()
+        elif client.is_paused:
+            await message.channel.send("```Resumed playback```")
+            client.is_paused = False
+            client.is_playing = True
+            client.vc.resume()
+    elif message.content.startswith('/resume'):
+        if client.is_paused:
+            await message.channel.send("```Resumed playback```")
+            client.is_paused = False
+            client.is_playing = True
+            client.vc.resume()
+    elif message.content.startswith('/remove'):
+        if len(client.music_queue) > 0:
+            client.music_queue.pop()
+            await message.channel.send("```Last song removed from queue```")
+        else:
+            await message.channel.send("```Queue is empty! Maybe you want to use /stop```")
 
 
 client.run(BOT_TOKEN)
